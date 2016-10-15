267771a622ed15cdb6376aa0ab3488d146fd8b61
Marie Yatsyk <maryats@users.noreply.github.com>
2016-10-15 13:37:40 -0400

    created getWeather() function to display weather for Boston

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,18 @@
+$(document).ready(function() {
+  getWeather();
+});
+
+function getWeather() {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&id=4930956&APPID=' + apiKey;
+  $.ajax(url, {
+    success: function (data) {
+      $('.city').text(data.name);
+      $('.temp').text(data.main.temp + ' °F');
+    }
+  });
+}
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
+  document.getElementById("demo").innerHTML = "Hey it's a new paragraph wow.";
 }
 
